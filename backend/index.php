cc03363617a8c5a57f3fcda9e353870213155e9d
Stousn <stefan.reip@edu.fh-joanneum.at>
2017-02-15 17:07:38 +0100

    changes from min to max PM10 value

--- backend/index.php
+++ backend/index.php
@@ -13,18 +13,19 @@ $today = new DateTime();
 $yesterday = new DateTime();
 $yesterday->add(DateInterval::createFromDateString('yesterday'));
 
-if (file_exists($cacheFile)) {
-    $cachDate = (new DateTime())->setTimestamp(filemtime($cacheFile));
-    if ($today->diff($cachDate)->days === 0) {
-        echo file_get_contents($cacheFile);
-        exit;
-    }
-}
+// if (file_exists($cacheFile)) {
+//     $cachDate = (new DateTime())->setTimestamp(filemtime($cacheFile));
+//     if ($today->diff($cachDate)->days === 0) {
+//         echo file_get_contents($cacheFile);
+//         exit;
+//     }
+// }
 
 $url = 'http://app.luis.steiermark.at/luft2/auswertung.php' .
   //  '?station1=143&station2=&komponente1=125&station3=&station4=&komponente2=' .
     '?station1=143&station2=142&komponente1=125&station3=141&station4=&komponente2=125'.
-    '&von_tag=' . $yesterday->format('j') . '&von_monat=' . $yesterday->format('n') . '&von_jahr=' . $yesterday->format('Y') . '&mittelwert=21' .
+  //  &von_tag=' . $yesterday->format('j') . '&von_monat=' . $yesterday->format('n') . '&von_jahr=' . $yesterday->format('Y') . '&mittelwert=21' .
+    '&von_tag=' . $yesterday->format('j') . '&von_monat=' . $yesterday->format('n') . '&von_jahr=' . $yesterday->format('Y') . '&mittelwert=15' .
     '&bis_tag=' . $today->format('j') . '&bis_monat=' . $today->format('n') . '&bis_jahr=' . $today->format('Y');
 
 $ch = curl_init();
@@ -42,7 +43,7 @@ $dom->loadHTML($html);
 libxml_use_internal_errors($internalErrors);
 
 $xpath = new DOMXPath($dom);
-$value = $xpath->query('//table[1]//table[3]//tr[4]//td[2]/text()')->item(0)->nodeValue;
+$value = $xpath->query('//table[1]//table[3]//tr[6]//td[2]/text()')->item(0)->nodeValue;
 
 $result = ['value' => $value];
 
